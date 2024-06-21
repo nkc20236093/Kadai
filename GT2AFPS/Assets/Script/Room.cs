d242eecbc322d3a2a0ab35d2c4bc2e7469dcd390
nkc20236093 <nck23.taki.eita@gmail.com>
2024-06-21 11:00:01 +0900

    オンゲFPS

---
+++ GT2AFPS/Assets/Script/Room.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.UI;
+using Photon.Realtime;
+
+public class Room : MonoBehaviour
+{
+    public Text buttonText;//ルーム名反映用のテキスト
+    private RoomInfo info;// 部屋の情報を格納する変数
+
+
+
+    /// <summary>
+    /// ルームボタンに詳細を登録
+    /// </summary>
+    public void RegisterRoomDetails(RoomInfo info)
+    {
+        this.info = info;//ルームのinfoに引数のinfoを格納
+
+        buttonText.text = this.info.Name;//テキスト更新
+    }
+}
