2f32e6ff99e8df0220de762bc0b9a230b78f4e67
nkc20236093 <nck23.taki.eita@gmail.com>
2025-10-23 11:42:06 +0900

    C++ 田島先生

--- practice/C++/3年/後期/Sample003/Sample003/Sample003/StartScene.cpp
+++ practice/C++/3年/後期/Sample003/Sample003/Sample003/StartScene.cpp
@@ -71,6 +71,16 @@ void StartScene::Init()
 
 SCENE StartScene::Update()
 {
+	//練習問題
+
+//	課題の事前準備として
+//	１．StartScene.cppを開く
+//	２．Enterキーのシーン遷移を無効化する
+//	３．ポリゴンの上下移動を停止させる
+
+//	※Matrixクラスの利用はどちらでも良い
+//	DirectXのクラスをそのまま利用しても良いし、先生が用意したMatrixクラスを利用しても良い
+
 	// シーン継続する場合はSCENE_MAXを返す
 	SCENE rt = SCENE_MAX;
 
@@ -96,18 +106,30 @@ SCENE StartScene::Update()
 	switch (mode)
 	{
 		case 1:
+			//	Lesson1 移動
+			//	x：1.0f 、y： 0.3f 、z：1.0f に移動させる
 			startMatrix.SetPos(XMFLOAT3(1.0f, 0.3f, 1.0f));
 			break;
 		case 2:
+			//	Lesson2 拡大
+			//	x：2.1f 、y：0.2f 、z：1.0f 拡大縮小させる
 			startMatrix.SetScale(XMFLOAT3(2.1f, 0.2f, 1.0f));
 			break;
 		case 3:
+			//	Lesson3 回転
+			//	x軸で60度回転させる
 			startMatrix.SetRotationX(60);
 			break;
 		case 4:
+			//	Lesson4 拡大と移動
+			//	x : 1.9f, y : 0.8f, z : 1.0f に拡大
+			//	x : -0.7f, y : 0.6f, z : -3.0f に移動
 			startMatrix.SetScale(XMFLOAT3(1.9f, 0.8f, 1.0f)),startMatrix.SetPos(XMFLOAT3(-0.7f, 0.6f, -3.0f));
 			break;
 		case 5:
+			//	Lesson5 回転と移動
+			//	z軸で常に回転（速度は自由 サンプルはconstexpr float SPEED = 0.01f; を利用している）
+			//	x : 1.0f, y : 0.5f, z : -3.0f に移動
 			Angle++;
 			if (Angle >= 360)
 			{
@@ -116,6 +138,10 @@ SCENE StartScene::Update()
 			startMatrix.SetRotationZ(XMConvertToRadians(Angle)), startMatrix.SetPos(XMFLOAT3(1.0f, 0.5f, -3.0f));
 			break;
 		case 6:
+			//	Lesson6 キー入力で移動
+			//	矢印キーで移動させる（速度は自由 サンプルはconstexpr float SPEED = 0.01f; を利用している）
+			//	正規化を行い、斜め移動にも対応させる
+
 			if (input->GetKey(VK_LEFT))  movePLPos.x -= 1 * Speed;
 			if (input->GetKey(VK_RIGHT)) movePLPos.x += 1 * Speed;
 			if (input->GetKey(VK_UP))    movePLPos.y += 1 * Speed;
@@ -127,6 +153,10 @@ SCENE StartScene::Update()
 			startMatrix.SetPos(movePLPos);
 		break;
 		case 7:
+			//	Lesson7 ポリゴン2つ目表示 WASDキーで移動
+			//	Lesson6の実装を残したまま追加でポリゴンを表示させる（初期位置はx : 1.0f, y : 0.0f, z : 0.0f）
+			//	WASDキーで移動させる（速度は自由 サンプルはconstexpr float SPEED
+
 			isSecond = true;
 			if (input->GetKey('A'))  moveSecondPos.x -= 1 * Speed;
 			if (input->GetKey('D')) moveSecondPos.x += 1 * Speed;
@@ -149,6 +179,11 @@ SCENE StartScene::Update()
 			startMatrix.SetPos(movePLPos);
 		break;
 		case 8:
+			//	Lesson8 スペースキーでポリゴンの崩壊
+			//	ちょっと難しい挑戦問題
+			//	Lesson7まで出来た人は挑戦してみよう
+			//	まずは周りと相談せずに一人で考えてみよう
+			
 			startMatrix.SetScale(XMFLOAT3(4.0f, 4.0f, 0));
 			if (Input::GetInstance()->GetKeyDown(VK_SPACE))
 			{
